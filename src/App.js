e57d6e840d5b3e89239f915b1f6325b21a895a6c
SsimbwaJoshua <ssimbwajoshua124@gmail.com>
2023-08-03 08:57:16 +0300

    split with section

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -75,6 +76,33 @@ function FormAddFriend() {
       <label>🐥 Image url</label>
       <input type="text" />
       <Button>Add </Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split the bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>💰 Your expenses </label>
+      <input type="text" />
+
+      <label>💰 Clerk's expense </label>
+      <input type="text" />
+
+      <label>💰 X's expense </label>
+      <input type="text" disabled />
+
+      <label>🙈 Who is paying the bill?</label>
+      <select>
+        <option value={"user"}>You</option>
+        <option value={"friend"}>X</option>
+      </select>
+      <Button>Split bill</Button>
     </form>
   );
 }
